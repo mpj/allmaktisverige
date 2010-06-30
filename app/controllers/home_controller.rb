b478f205d782016f1854570dcf5ce6d1c2164708
Mattias Petter Johansson <mpj@mpj.me>
2010-06-30 22:20:19 +0200

    hey3

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
+    @authority = self.request.host
+    if self.request.port != 80
+      @authority = @authority + ":" + self.request.port.to_s
+    end
+
   end
 
 end
